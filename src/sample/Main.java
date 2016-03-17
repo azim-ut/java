8595b70aa0eefe3669887becbc678d217a85fbe3
Aleksandr Kostiukov <akm@leongaming.com>
2016-03-17 10:21:14 +0200

    root tutorial: just scene with placed button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,31 +1,27 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import pane.AuthorisationStackPane;
 
 public class Main extends Application {
-    private Scene authorisationScene;
-    private Parent root;
+    private Button button;
 
     @Override public void start(Stage primaryStage) throws Exception {
-        root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(getAuthorisationScene());
+
+        button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 800, 600);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-
-    private Scene getAuthorisationScene() {
-        if (authorisationScene == null) {
-            authorisationScene = new Scene(new AuthorisationStackPane(), 300, 250);
-        }
-        return authorisationScene;
-    }
-
     public static void main(String[] args) {
         launch(args);
     }
