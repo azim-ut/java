43818e41caa66c62ec05779d5aaa597890595e90
mvkent <mvkent@mail.ru>
2016-04-23 01:46:34 +0300

    Не смог запустить. Хоть что-то отправлю... Буду разбираться

--- src/sample/utils/FileUtils.java
+++ src/sample/utils/FileUtils.java
@@ -27,4 +27,37 @@ public class FileUtils {
         }
         return stringBuilder.toString();
     }
+
+    /*public static void writeStringToFile (File file,String data,boolean append);
+    String string = "This is\na new test";
+    File file = new File("test.txt");
+    FileUtils.writeStringToFile(file, string);*/
+
+    public static void write(String fileName, String text) {
+        //Определяем файл
+        File file = new File(fileName);
+
+        try {
+            //проверяем, что если файл не существует то создаем его
+            if(!file.exists()){
+                file.createNewFile();
+            }
+
+            //PrintWriter обеспечит возможности записи в файл
+            PrintWriter out = new PrintWriter(file.getAbsoluteFile());
+
+            try {
+                //Записываем текст у файл
+                out.print(text);
+            } finally {
+                //После чего мы должны закрыть файл
+                //Иначе файл не запишется
+                out.close();
+            }
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
