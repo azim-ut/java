dfcf5d87592ef2434aba9f331afa4f84e5059bfc
Aleksandr Kostiukov <akm@leongaming.com>
2017-01-04 23:33:10 +0200

    text parser implemented

--- src/main/java/controllers/FinanceRecordFormController.java
+++ src/main/java/controllers/FinanceRecordFormController.java
@@ -1,21 +1,44 @@
 package controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.text.Text;
+import javafx.scene.control.TextArea;
 
 /**
  * Created by Aleksandr Kostyukov on 5/19/2016.
  */
 public class FinanceRecordFormController{
     @FXML
-    private Text actionTarget;
+    private TextArea textInput, textResult;
 
     @FXML
-    protected void handleAddFinanceRow(ActionEvent event){
-        actionTarget.setText("Adok");
+    protected void parseText(ActionEvent event){
+        String result = textInput.getText();
+        String[] list = result.split(" ");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        Map<String, Number> wordsMap = new HashMap<String, Number>();
+
+        for (String word : list) {
+            if(!wordsMap.containsKey(word)){
+                wordsMap.put(word, 0);
+            }else{
+                wordsMap.put(word, (wordsMap.get(word).intValue() + 1));
+            }
+        }
+
+        //iterate words map
+        for (Map.Entry<String, Number> entry : wordsMap.entrySet()) {
+            String key = entry.getKey();
+            Number value = entry.getValue();
+            System.out.println(key + "-" + value);
+        }
+
+        //sort words map by value  - google request - java map comparator
+
+
+        textResult.setText(stringBuilder.toString());
     }
-
-
 }
